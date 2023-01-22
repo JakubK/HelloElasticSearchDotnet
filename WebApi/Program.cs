f2a9ad7e9f85b3f524d2f9ddd094910e1f3d1dc0
Jakub Kuniszewski <you@example.com>
2023-01-22 13:47:07 +0100

    Add Serilog with Elastic configuration and update appsettings

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,26 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ctx.Configuration["ElasticConfiguration:Uri"]))
+        {
+            IndexFormat =
+                $"{ctx.Configuration["ApplicationName"]}-logs-{ctx.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfShards = 2,
+            NumberOfReplicas = 1
+        })
+        .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName!)
+        .ReadFrom.Configuration(ctx.Configuration);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
